89c5447124797ab030b5513035a56353a620cef3
Jay <apple@apples-MacBook-Pro.local>
2024-08-10 18:33:51 +0530

    intance vpc_id field

--- vpc.tf
+++ vpc.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "public" {
   instance_type               = var.instance_type
   subnet_id                   = module.vpc.public_subnets[0]
   key_name                    = var.key_name
-  security_groups             = [aws_security_group.public_sg.id]
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
   tags                        = var.instance_tags_1
 }
